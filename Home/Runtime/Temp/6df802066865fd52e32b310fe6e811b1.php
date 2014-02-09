4a5577fc05fdfd5baa5e3ad871b19ee47e6bc694
David <david@t.com>
2014-02-09 19:07:25 +0800

    v 1.2

--- Home/Runtime/Temp/6df802066865fd52e32b310fe6e811b1.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000000001a:1:{i:0;a:8:{s:2:"id";s:3:"415";s:7:"num_iid";s:11:"27106492384";s:5:"title";s:55:"童装 HEATTECH紧身裤(横条) 079741 优衣库UNIQLO";s:3:"num";s:3:"186";s:5:"price";s:5:"79.00";s:7:"pic_url";s:73:"Upload/goods/2013/2013-10/2013-10-21/35ce431a3ab0ab40e7748ce4cde8bf95.png";s:10:"detail_url";s:68:"http://item.taobao.com/item.htm?id=27106492384&spm=2014.21366151.0.0";s:7:"ccateid";s:2:"79";}}
-?>
