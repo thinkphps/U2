f0c404fc7cbd5ed246fcff82513ed3cc4f9830ec
leon.li <brando_beat@126.com>
2014-02-11 20:19:38 +0800

    2014/1/27

--- mini.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('APP_NAME','Mini');
-define('APP_PATH',APP_NAME.'/');
-define('APP_DEBUG',true);
-require( "./ThinkPHP/ThinkPHP.php");
--- system.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('APP_NAME', 'System');
-define('APP_PATH', APP_NAME.'/');
-define('APP_DEBUG', true);
-// 加载框架入口文件
-require( "./ThinkPHP/ThinkPHP.php");
