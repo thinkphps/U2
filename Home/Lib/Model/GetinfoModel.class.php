5d75aa34f92a7f3b86fc69917e09f4c41840e37d
qwqw321 <565824abc>
2014-04-24 16:39:01 +0800

    home

--- Home/Lib/Model/GetinfoModel.class.php
+++ Home/Lib/Model/GetinfoModel.class.php
@@ -147,7 +147,7 @@ class GetinfoModel extends Action{
         $result = M('Suits')->cache(true)->join('left join u_settings_suit_style as g on u_suits.suitStyleID=g.ID')->field('u_suits.suitID,u_suits.suitGenderID,u_suits.suitImageUrl,g.description')->where($where)->order('u_suits.suitID desc')->select();
         $goodsDetail = M('SuitsGoodsdetail');
         foreach($result as $k=>$v){
-            $result[$k]['detail'] = $goodsDetail->cache(true)->join('inner join u_beubeu_goods ug on u_suits_goodsdetail.num_iid=ug.num_iid')->field('ug.num_iid,ug.pic_url,ug.detail_url,ug.title')->where(array('u_suits_goodsdetail.suitID'=>$v['suitID']))->select();
+            $result[$k]['detail'] = $goodsDetail->cache(true)->join('inner join u_beubeu_goods ug on u_suits_goodsdetail.num_iid=ug.num_iid')->field('ug.num_iid,ug.pic_url,ug.detail_url,ug.title')->where(array('u_suits_goodsdetail.suitID'=>$v['suitID'],'ug.approve_status'=>'onsale','ug.num'=>array('egt','15')))->select();
         }
         return $result;
     }
