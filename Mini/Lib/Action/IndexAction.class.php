368afbe50a9cf40d3b21fdd6a444767d49851d18
qwqw321 <565824abc>
2014-04-25 17:44:26 +0800

    mini

--- Mini/Lib/Action/IndexAction.class.php
+++ Mini/Lib/Action/IndexAction.class.php
@@ -245,7 +245,7 @@ public function getgood(){
        $result = unserialize(S('coll'.session("uniq_user_id").$page));
      }else{
      $collection = M('Collection');
-      $result = $collection->join('inner join u_beubeu_goods bg on bg.num_iid=u_collection.num_iid')->field('bg.num_iid,bg.type,bg.title,bg.num,bg.price,bg.pic_url,bg.detail_url')->order('u_collection.id desc')->where(array('u_collection.uid'=>session("uniq_user_id")))->limit($start.','.$page_num)->select();
+      $result = $collection->join('inner join u_beubeu_goods bg on bg.num_iid=u_collection.num_iid')->field('bg.num_iid,bg.type,bg.isud,bg.title,bg.num,bg.price,bg.pic_url,bg.detail_url')->order('u_collection.id desc')->where(array('u_collection.uid'=>session("uniq_user_id")))->limit($start.','.$page_num)->select();
          foreach($result as $k1=>$v1){
              $result[$k1]['products'] = $productSyn->GetProductColorByID($v1['num_iid']);
          }
@@ -260,7 +260,7 @@ public function getgood(){
         $result = unserialize(S('buy'.session("uniq_user_id").$page));
     }else{
         $buy = M('Buy');
-        $result = $buy->join('inner join u_beubeu_goods bg on bg.num_iid=u_buy.num_iid')->field('bg.num_iid,bg.type,bg.title,bg.num,bg.price,bg.pic_url,bg.detail_url')->order('u_buy.id desc')->where(array('u_buy.uid'=>session("uniq_user_id")))->limit($start.','.$page_num)->select();
+        $result = $buy->join('inner join u_beubeu_goods bg on bg.num_iid=u_buy.num_iid')->field('bg.num_iid,bg.type,bg.isud,bg.title,bg.num,bg.price,bg.pic_url,bg.detail_url')->order('u_buy.id desc')->where(array('u_buy.uid'=>session("uniq_user_id")))->limit($start.','.$page_num)->select();
         foreach($result as $k1=>$v1){
             $result[$k1]['products'] = $productSyn->GetProductColorByID($v1['num_iid']);
         }
@@ -272,7 +272,7 @@ public function getgood(){
     }else if($kid>0){
        //快速搜索走这里
         if($keyword){
-       $result = M('BeubeuGoods')->field('u_beubeu_goods.num_iid,u_beubeu_goods.type,u_beubeu_goods.title,u_beubeu_goods.num,u_beubeu_goods.price,u_beubeu_goods.pic_url,u_beubeu_goods.detail_url')->where(array('approve_status'=>'onsale','num'=>array('egt','15'),'title'=>array('like','%'.$keyword.'%')))->order('uptime desc')->limit($start.','.$page_num)->select();
+       $result = M('BeubeuGoods')->field('u_beubeu_goods.num_iid,u_beubeu_goods.type,u_beubeu_goods.isud,u_beubeu_goods.title,u_beubeu_goods.num,u_beubeu_goods.price,u_beubeu_goods.pic_url,u_beubeu_goods.detail_url')->where(array('approve_status'=>'onsale','num'=>array('egt','15'),'title'=>array('like','%'.$keyword.'%')))->order('uptime desc')->limit($start.','.$page_num)->select();
       }
         foreach($result as $k1=>$v1){
             $result[$k1]['products'] = $productSyn->GetProductColorByID($v1['num_iid']);
@@ -328,7 +328,7 @@ public function getgood(){
                   $case = '';
                   $ordr = "order by ";
                 }
-               $sql = "select g.good_id".$case.", bg.num_iid,bg.type,bg.title,bg.num,bg.price,bg.pic_url,bg.detail_url from `u_goodtag` as g inner join `u_beubeu_goods` as bg on bg.id=g.good_id where 1 ".$where." group by g.good_id ".$ordr."uptime desc limit ".$start.",".$page_num;
+               $sql = "select g.good_id".$case.", bg.num_iid,bg.type,bg.isud,bg.title,bg.num,bg.price,bg.pic_url,bg.detail_url from `u_goodtag` as g inner join `u_beubeu_goods` as bg on bg.id=g.good_id where 1 ".$where." group by g.good_id ".$ordr."uptime desc limit ".$start.",".$page_num;
             $result = $goodtag->query($sql);
             foreach($result as $k1=>$v1){
                 $result[$k1]['products'] = $productSyn->GetProductColorByID($v1['num_iid']);
