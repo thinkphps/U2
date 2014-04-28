0a26eae37e14eea2fc14b9aa4051799dce6d9a8c
jack <106586908@qq.com>
2014-04-28 14:27:47 +0800

    已收藏、已购买样式

--- Mini/Lib/Action/IndexAction.class.php
+++ Mini/Lib/Action/IndexAction.class.php
@@ -254,7 +254,7 @@ public function getgood(){
              $result = array();
          }
          if($page==1){
-             $result = $this->waterdata($result);
+             $result = $this->waterdata($result,$lid,$bid);
          }
     S('coll'.session("uniq_user_id").$page,serialize($result),array('type'=>'file'));
     }
@@ -273,7 +273,7 @@ public function getgood(){
             $result = array();
         }
         if($page==1){
-            $result = $this->waterdata($result);
+            $result = $this->waterdata($result,$lid,$bid);
         }
         S('buy'.session("uniq_user_id").$page,serialize($result),array('type'=>'file'));
     }
@@ -290,7 +290,7 @@ public function getgood(){
             $result = array();
         }
         if($page==1){
-            $result = $this->waterdata($result);
+            $result = $this->waterdata($result,$lid,$bid);
         }
      }else{
         //普通走这里
@@ -350,7 +350,7 @@ public function getgood(){
                 $result = array();
             }
             if($page==1){
-                $result = $this->waterdata($result);
+                $result = $this->waterdata($result,$lid,$bid);
             }
             S('good'.$sid.$fid.$zid.$tem.$page,serialize($result),array('type'=>'file'));
            }
@@ -363,12 +363,24 @@ public function getgood(){
      }
     $this->ajaxReturn($returnArr, 'JSON');
 }
-    public function waterdata($result){
+    public function waterdata($result,$lid,$bid){
         $ad = "<div class='productinfo wrapper_box banner_box'><a href='javascript:;'><img src='".__ROOT__."/".APP_PATH."Tpl/Public/images/xsyh.jpg' width='228' height='471' alt='' /></a></div>";
         $ad2 = '<div class="productinfo wrapper_box banner_box"><a href="javascript:;"><img src="'.__ROOT__."/".APP_PATH.'Tpl/Public/images/xinzuoshangpin.jpg" width="228" height="228" alt="" /></a></div>';
 
+        if($lid == 1 && $bid == 0){
+            $str = '<div class="productinfo"><div class="wrapper_box wrapper_box_btn_group"><a href="javascript:;" class="ysc_btn select" id="cldata"><i></i>已收藏</a><a href="javascript:;" class="ygm_btn" id="buydata"><i></i>已购买</a></div><div class="wrapper_box wrapper_box_search"><input name="search" type="text" value="" placeholder="输入您想要的款式或名称" autocomplete="off" id="keywordid"><a href="javascript:;" id="keybutton"></a></div></div>';
+        }
+        else if($lid == 0 && $bid == 1){
+            $str = '<div class="productinfo"><div class="wrapper_box wrapper_box_btn_group"><a href="javascript:;" class="ysc_btn" id="cldata"><i></i>已收藏</a><a href="javascript:;" class="ygm_btn select" id="buydata"><i></i>已购买</a></div><div class="wrapper_box wrapper_box_search"><input name="search" type="text" value="" placeholder="输入您想要的款式或名称" autocomplete="off" id="keywordid"><a href="javascript:;" id="keybutton"></a></div></div>';
 
-        $str = '<div class="productinfo"><div class="wrapper_box wrapper_box_btn_group"><a href="javascript:;" class="ysc_btn" id="cldata"><i></i>已收藏</a><a href="javascript:;" class="ygm_btn" id="buydata"><i></i>已购买</a></div><div class="wrapper_box wrapper_box_search"><input name="search" type="text" value="" placeholder="输入您想要的款式或名称" autocomplete="off" id="keywordid"><a href="javascript:;" id="keybutton"></a></div></div>';
+        }else if($lid == 1 && $bid == 1){
+            $str = '<div class="productinfo"><div class="wrapper_box wrapper_box_btn_group"><a href="javascript:;" class="ysc_btn select" id="cldata"><i></i>已收藏</a><a href="javascript:;" class="ygm_btn select" id="buydata"><i></i>已购买</a></div><div class="wrapper_box wrapper_box_search"><input name="search" type="text" value="" placeholder="输入您想要的款式或名称" autocomplete="off" id="keywordid"><a href="javascript:;" id="keybutton"></a></div></div>';
+        }else if($lid == 0 && $bid == 0 ){
+            $str = '<div class="productinfo"><div class="wrapper_box wrapper_box_btn_group"><a href="javascript:;" class="ysc_btn" id="cldata"><i></i>已收藏</a><a href="javascript:;" class="ygm_btn" id="buydata"><i></i>已购买</a></div><div class="wrapper_box wrapper_box_search"><input name="search" type="text" value="" placeholder="输入您想要的款式或名称" autocomplete="off" id="keywordid"><a href="javascript:;" id="keybutton"></a></div></div>';
+        }
+        else{
+            $str = '<div class="productinfo"><div class="wrapper_box wrapper_box_btn_group"><a href="javascript:;" class="ysc_btn" id="cldata"><i></i>已收藏</a><a href="javascript:;" class="ygm_btn" id="buydata"><i></i>已购买</a></div><div class="wrapper_box wrapper_box_search"><input name="search" type="text" value="" placeholder="输入您想要的款式或名称" autocomplete="off" id="keywordid"><a href="javascript:;" id="keybutton"></a></div></div>';
+        }
 
 //        $str = '<div class="wrapper_box wrapper_box_btn_group"><a href="javascript:;" class="ysc_btn" id="cldata"><i></i>已收藏</a><a href="javascript:;" class="ygm_btn" id="buydata"><i></i>已购买</a><div class="wrapper_box_search"><input name="search" type="text" value="" placeholder="输入您想要的款式或名称" autocomplete="off" id="keywordid"><a href="javascript:;" id="keybutton"></a></div></div>';
         array_unshift($result,array('first'=>1,'ad'=>$ad));
