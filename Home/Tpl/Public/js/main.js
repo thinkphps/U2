06db9020c883146d2d33a29d36bcc70044d38599
jack <106586908@qq.com>
2014-05-04 17:45:07 +0800

    首页增加 天猫code

--- Home/Tpl/Public/js/main.js
+++ Home/Tpl/Public/js/main.js
@@ -132,7 +132,7 @@ var timer;
                 var suitid = $(this).data('suitid');
                 var gender = $(this).data('gender');
                 //jsonpHomeUrl
-                window.open( baseurl + 'mini.php/Index?suitid='+ suitid + '&gender=' + gender);
+                window.open(  'http://a1761.oadz.com/link/C/1761/721/CxPszkT1lM4WsFgzgD1ae-O3Pqo_/p06d/0/http://j.koolbao.com/tb/57303596/' +baseurl + 'mini.php/Index?kid=11727_51912_151244_195188&suitid='+ suitid + '&gender=' + gender);
             });
 
             // Ê×Ò³ÌìÆøÇĞ»»
@@ -418,7 +418,9 @@ var callBackFunction = {
         strItem +='</div>';
         strItem += '<div class="itemTitle">'+ this.getStyleByDescription(item.description)+'</div>';//<br><font style="color: #C0C0C0">'+ item.eglishName+'</font>
         strItem += '<div class="gotoroom none">';
-        var url = baseurl + 'mini.php/Index?suitid='+ item.suitID + '&gender=' + item.suitGenderID ;
+
+        var url = 'http://a1761.oadz.com/link/C/1761/721/CxPszkT1lM4WsFgzgD1ae-O3Pqo_/p06d/0/http://j.koolbao.com/tb/57303596/'
+            + baseurl + 'mini.php/Index?kid=11727_51912_151244_195188&suitid='+ item.suitID + '&gender=' + item.suitGenderID ;
         strItem += '<a href="'+ url + '" target="_blank">È¥ĞéÄâÊÔÒÂ¼äÊÔ´©</a></div></div>';
         return strItem;
     },
