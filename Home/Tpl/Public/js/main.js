1c143f5248743fa8cc1f451c1c07ad2b7b54c9ee
jack <106586908@qq.com>
2014-05-06 14:29:34 +0800

    使用JS跳转页面

--- Home/Tpl/Public/js/main.js
+++ Home/Tpl/Public/js/main.js
@@ -434,7 +434,7 @@ var callBackFunction = {
         strItem += '<div class="gotoroom none">';
 
         var url ='http://a1761.oadz.com/link/C/1761/721/CxPszkT1lM4WsFgzgD1ae-O3Pqo_/p06d/0/http://j.koolbao.com/tb/57303596/uniqlo.bigodata.com.cn/u2/mini.php/Index.html/?kid=11727_51912_151244_195188&suitid='+ item.beubeuSuitID + '&gender=' + item.suitGenderID ;
-        strItem += '<a href="#" data-dressurl="'+ url + '" class="dressurl" target="_blank">È¥ĞéÄâÊÔÒÂ¼äÊÔ´©</a></div></div>';
+        strItem += '<a href="javascript:;" data-dressurl="'+ url + '" class="dressurl" target="_blank">È¥ĞéÄâÊÔÒÂ¼äÊÔ´©</a></div></div>';
         return strItem;
     },
     getStyleByDescription : function(description){
