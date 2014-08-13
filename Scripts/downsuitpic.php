70af94462e680af95722aec44075eb5acffac80d
qwqw321 <565824abc>
2014-08-13 14:55:53 +0800

    下载图片

--- Scripts/downsuitpic.php
+++ Scripts/downsuitpic.php
@@ -21,13 +21,13 @@ while($ke>0){
         $time = date('Y-m-d H:i:s');
         foreach($result as $k2=>$v2){
             $image_head = $db->createdir($v2['suitID'].'_Head',$root_dir.'/Upload/partsuits/Head/','Upload/partsuits/Head/',$v2['suitImageUrlHead'],2);
-            @file_put_contents($image_head[0], file_get_contents($v2['suitImageUrlHead']));//头
+            @file_put_contents($image_head[0], file_get_contents($v2['suitImageUrlHead'].'.600x800.png'));//头
             $image_body = $db->createdir($v2['suitID'].'_Body',$root_dir.'/Upload/partsuits/Body/','Upload/partsuits/Body/',$v2['suitImageUrlBody'],2);
-            @file_put_contents($image_body[0], file_get_contents($v2['suitImageUrlBody']));//身体
+            @file_put_contents($image_body[0], file_get_contents($v2['suitImageUrlBody'].'.600x800.png'));//身体
             $image_shose = $db->createdir($v2['suitID'].'_Shose',$root_dir.'/Upload/partsuits/Shose/','Upload/partsuits/Shose/',$v2['suitImageUrlShose'],2);
-            @file_put_contents($image_shose[0], file_get_contents($v2['suitImageUrlShose']));//鞋子
+            @file_put_contents($image_shose[0], file_get_contents($v2['suitImageUrlShose'].'.600x800.png'));//鞋子
             $image_match = $db->createdir($v2['suitID'].'_Match',$root_dir.'/Upload/partsuits/Match/','Upload/partsuits/Match/',$v2['suitImageUrlMatch'],2);
-            @file_put_contents($image_match[0], file_get_contents($v2['suitImageUrlMatch']));//衣服
+            @file_put_contents($image_match[0], file_get_contents($v2['suitImageUrlMatch'].'.600x800.png'));//衣服
             $resql = "select `suitid` from `u_suit_pic` where `suitid`=".$v2['suitID'];
             $re = $db->mysqlfetch($resql);
             if(empty($re)){
