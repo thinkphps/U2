ce9bc672ff51ccbdf0feec430c5f39bf0b93ffc7
qwqw321 <565824abc>
2014-08-19 11:51:19 +0800

    首页默认过来显示女性模特

--- Mini/Tpl/Public/js/modeldress.js
+++ Mini/Tpl/Public/js/modeldress.js
@@ -135,7 +135,12 @@ function Model_loadok_callback(){
             /***新的百衣搭配间****/
             var touchid= 854;
             var key="8f1a6e3f182904ad22170f56c890e533";
-            loadMymodel(touchid,key,15478);
+            var suitid = pageElement.getUrlParam('suitid');
+            if(suitid!=-1){
+                loadMymodel(touchid,key,15478);
+            }else{
+                loadMymodel(touchid,key,15474);
+            }
             //loadMymodel(858,'165ea085e3da6182e441b472989468fc');
             Model.CurrClothesCallback = this.beu_getallclothes;
             $('.beubeu_btns').css('left','25px');
