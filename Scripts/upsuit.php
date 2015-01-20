a6308a8e70b61d49442cb038eb8a7cb53bbf9e9e
qwqw321 <565824abc>
2015-01-20 17:28:53 +0800

    更新内页模特图

--- Scripts/upsuit.php
+++ Scripts/upsuit.php
@@ -20,7 +20,7 @@ while($ke>0){
         foreach($result as $i=>$suit){
             $item_arr = array();
             //如果百衣推过来的图片数据里边有没有上架的商品，则此模特图不显示
-            $usql = "select udg.item_bn from `u_beubeu_suits_goodsdetail` udg inner join `u_beubeu_goods` ugs on left(ugs.`item_bn`,8) = left(udg.`item_bn`,8) where  udg.suitID=".$suit['suitID'];
+            $usql = "select udg.item_bn from `u_beubeu_suits_goodsdetail` udg inner join `u_beubeu_goods` ugs on left(ugs.`item_bn`,8) = left(udg.`item_bn`,8) where  udg.suitID=".$suit['suitID']." and ugs.approve_status = 'onsale'";
             $item_result = $db->mysqlfetch($usql);
             $desql = "select item_bn from `u_beubeu_suits_goodsdetail` where suitID=".$suit['suitID']." and left(item_bn,2)='UQ'";
             $dresult = $db->mysqlfetch($desql);
@@ -30,23 +30,17 @@ while($ke>0){
                 }
             }
             //只有要一个num_iid不能显示，则这个图片就不能显示
+            $i = 0;//可以显示
             foreach($dresult as $k2=>$v2){
                 if(!in_array($v2['item_bn'],$item_arr)){
-                    $sql123 = "update `u_beubeu_suits` set `approve_status`=1 , `uptime`='".$currentDateTime."' where `suitID`=".$suit['suitID'];
+                    $sql123 = "update `u_beubeu_suits` set `approve_status`=1, `uptime`='".$currentDateTime."' where `suitID`=".$suit['suitID'];
                     $db->mysqlquery($sql123);
+                    $i = 1;//不能显示
                     break;
                 }
             }
-            $i = 0;//可以显示
-            foreach($dresult as $k2=>$v2){
-                if(in_array($v2['item_bn'],$item_arr)){
-                    $i = 0;
-                }else{
-                    $i = 1;//不能显示
-                }
-            }
             if($i==0){
-                $sql123 = "update `u_beubeu_suits` set `approve_status`=0 , `uptime`='".$currentDateTime."' where `suitID`=".$suit['suitID'];
+                $sql123 = "update `u_beubeu_suits` set `approve_status`=0, `uptime`='".$currentDateTime."' where `suitID`=".$suit['suitID'];
                 $db->mysqlquery($sql123);
             }
         }
