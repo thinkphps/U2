192b7509c2fb800f83c52e420b14ef4467f7affb
qwqw321 <565824abc>
2015-01-31 13:15:45 +0800

    增加判断文件是否存在

--- Scripts/downsuitpic.php
+++ Scripts/downsuitpic.php
@@ -12,7 +12,7 @@ $limit = 200;
 $ke = 1;
 while($ke>0){
     $sql = "select `suitID`,`suitImageUrlHead`,`suitImageUrlBody`,`suitImageUrlShose`,`suitImageUrlMatch` from `u_beubeu_suits` where suitImageUrl='' and `approve_status`=0 limit $offset,$limit";
-    //$sql = "select `suitID`,`suitImageUrlHead`,`suitImageUrlBody`,`suitImageUrlShose`,`suitImageUrlMatch` from `u_beubeu_suits` where 1 limit $offset,$limit";
+	//$sql = "select `suitID`,`suitImageUrlHead`,`suitImageUrlBody`,`suitImageUrlShose`,`suitImageUrlMatch` from `u_beubeu_suits` where `approve_status`=0 limit $offset,$limit";
     $result = $db->mysqlfetch($sql);
     unset($sql);
     if(empty($result)){
@@ -21,18 +21,26 @@ while($ke>0){
         $time = date('Y-m-d H:i:s');
         foreach($result as $k2=>$v2){
             $image_head = $db->createdir($v2['suitID'].'_Head',$root_dir.'/Upload/partsuits/Head/','Upload/partsuits/Head/',$v2['suitImageUrlHead'],2,$offset+1);
+			if(!filesize($image_head[0])){
             @file_put_contents($image_head[0], file_get_contents($v2['suitImageUrlHead'].'.600x800.png'));//头
+			}
             $image_body = $db->createdir($v2['suitID'].'_Body',$root_dir.'/Upload/partsuits/Body/','Upload/partsuits/Body/',$v2['suitImageUrlBody'],2,$offset+1);
+			if(!filesize($image_body[0])){
             @file_put_contents($image_body[0], file_get_contents($v2['suitImageUrlBody'].'.600x800.png'));//身体
+            }
 			$shoseexten = pathinfo($v2['suitImageUrlShose'],PATHINFO_EXTENSION);
 			if($shoseexten=='png'){
             $image_shose = $db->createdir($v2['suitID'].'_Shose',$root_dir.'/Upload/partsuits/Shose/','Upload/partsuits/Shose/',$v2['suitImageUrlShose'],2,$offset+1);
+			if(!filesize($image_shose[0])){
             @file_put_contents($image_shose[0], file_get_contents($v2['suitImageUrlShose'].'.600x800.png'));//鞋子
+            }
 		    }else{
               $image_shose[1] = 1;
 			}
             $image_match = $db->createdir($v2['suitID'].'_Match',$root_dir.'/Upload/partsuits/Match/','Upload/partsuits/Match/',$v2['suitImageUrlMatch'],2,$offset+1);
+			if(!filesize($image_match[0])){
             @file_put_contents($image_match[0], file_get_contents($v2['suitImageUrlMatch'].'.600x800.png'));//衣服
+			}
             $resql = "select `suitid` from `u_suit_pic` where `suitid`=".$v2['suitID'];
             $re = $db->mysqlfetch($resql);
             if(empty($re)){
