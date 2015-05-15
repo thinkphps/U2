39392673164e974f363c76d3c070190395d58bc1
qwqw321 <565824abc>
2015-05-15 14:36:09 +0800

    如果淘宝上删除了商品，则把categoods里对应的数据删除掉

--- Scripts/upstock.php
+++ Scripts/upstock.php
@@ -11,34 +11,26 @@ $c->secretKey = $db->secretKey;
 $c->format = 'json';
 $products = new ItemGetRequest;//获取商品详细信息
 $products->setFields('num,modified,approve_status,sku.quantity,sku.sku_id,sku.modified');
-//判断是否能取到数据
-
-$req = new ItemsOnsaleGetRequest;
-$req->setFields("num_iid,title,price");
-$req->setIsTaobao("true");
-$req->setPageSize(1);
-$resp = $c->execute($req, $db->token);
-if(isset($resp->items->item) && !empty($resp->items->item)){
-//判断是否能取到数据
-    $offset = 0;
-	$limit = 200;
-    $ke = 1;
-	while($ke>0){
+$offset = 0;
+$limit = 200;
+$ke = 1;
+while($ke>0){
     $sql = "select `id`,`num_iid`,`approve_status` from `u_goods` where 1 limit $offset,$limit";
-	$result = $db->mysqlfetch($sql);
-	unset($sql);
-	if(empty($result)){
-	$ke = 0;
-	}else{
-	foreach($result as $k=>$v){
-	$products->setNumIid($v['num_iid']);
-	$product = $c->execute($products, $db->token);
-	$product_arr = (array)$product->item;
+    $result = $db->mysqlfetch($sql);
+    unset($sql);
+    if(empty($result)){
+    $ke = 0;
+    }else{
+    foreach($result as $k=>$v){
+    $products->setNumIid($v['num_iid']);
+    $product = $c->execute($products, $db->token);
+    if($product->code!=530){
+    $product_arr = (array)$product->item;
     if(!empty($product_arr)){
     $product_arr_sku = (array)$product_arr['skus']->sku;
-	$sql = "update `u_goods` set `approve_status`='".$product_arr['approve_status']."',`num`='".$product_arr['num']."',`sort`='".$v['modified']."' where `id`='".$v['id']."'";
-	$db->mysqlquery($sql);
-	unset($sql);
+    $sql = "update `u_goods` set `approve_status`='".$product_arr['approve_status']."',`num`='".$product_arr['num']."',`sort`='".$v['modified']."' where `id`='".$v['id']."'";
+    $db->mysqlquery($sql);
+    unset($sql);
     //更新products表
     foreach($product_arr_sku as $k2=>$v2){
         $v2 = (array)$v2;
@@ -46,11 +38,15 @@ if(isset($resp->items->item) && !empty($resp->items->item)){
         $db->mysqlquery($psql);
      }
     }
-	}
-	}
-	$offset+=$limit;
-	sleep(2);
-	unset($result);
-	}
+    }else{
+     //淘宝商品已经删除则删除categoods表里的对应关系
+     $delcgsql = "delete from `u_catesgoods` where `num_iid`=".$v['num_iid'];
+     $db->mysqlquery($delcgsql);
+    }
+    }
+    }
+    $offset+=$limit;
+    sleep(2);
+    unset($result);
 }
 exit;
